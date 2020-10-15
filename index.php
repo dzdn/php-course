07d04d4d0a20c240980e3ea54f38ebe349def60c
J. Dustin Williams <jdustinwilliams@gmail.com>
2020-10-15 13:52:04 -0400

    add variables

--- index.php
+++ index.php
@@ -5,9 +5,10 @@
   </head>
   <body>
     <?php
-    // Using two forward slashes we create comments in php.
-    // Within the php tag, we can insert values into our HTML.
-    echo "<h1>Welcome!</h1>";
+        $name = "PHP Store";
+        $credit = 1000;
+
+        echo "<h1>Welcome to ".$name."!</h1>";
     ?>
   </body>
 </html>
